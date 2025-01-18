ce9ad2e49e4135dac6d3bdddefdee048e1ccd0c0
Maqi-x <smart.tech.yt.kontakt@gmail.com>
2025-01-18 12:37:56 +0100

    PhysicalObject itp.

--- src/Physical.go
+++ src/Physical.go
@@ -7,7 +7,6 @@ import (
 
 type PhysicalObj struct {
 	*BaseObject
-	Jumping      bool
 	LockPosition bool
 }
 
@@ -34,7 +33,6 @@ func NewPhysicalObj(renderer *sdl.Renderer, imagePath string, x, y int32) (*Phys
 			Height:   img.H,
 			Renderer: renderer,
 		},
-		Jumping: false,
 	}
 	physicalObjects = append(physicalObjects, nObj)
 	return nObj, nil
@@ -66,7 +64,6 @@ func NewPhysicalBaseObj(renderer *sdl.Renderer, color sdl.Color, x, y, w, h int3
 			Height:   h,
 			Renderer: renderer,
 		},
-		Jumping: false,
 	}
 	physicalObjects = append(physicalObjects, nObj)
 	return nObj, nil
@@ -84,8 +81,8 @@ func (self *PhysicalObj) PhysicTick() {
 	if !self.OnGround() && !self.LockPosition {
 		self.Y += int32(physic.GravtyPower)
 	}
-	if self.Y > 600 {
-		self.Y = 600
+	if self.Y > screen.Height {
+		self.Y = screen.Height
 	}
 }
 
@@ -95,8 +92,38 @@ func (self *PhysicalObj) OnGround() bool {
 			continue
 		}
 
-		if self.X < obj.X+obj.Width && self.X+self.Width > obj.X &&
-			self.Y+self.Height <= obj.Y+1 && self.Y+self.Height >= obj.Y {
+		fmt.Printf("Checking obj: X=%d, Y=%d, Width=%d, Height=%d\n", obj.X, obj.Y, obj.Width, obj.Height)
+		fmt.Printf("Self: X=%d, Y=%d, Width=%d, Height=%d\n", self.X, self.Y, self.Width, self.Height)
+
+		if self.X < obj.X+obj.Width && self.X+self.Width > obj.X && self.Y+self.Height >= obj.Y {
+			//fmt.Println("OnGround=True")
+			return true
+		}
+	}
+	fmt.Println("OnGround=False")
+	return false
+}
+
+func (self *PhysicalObj) CheckCollisionLeft() bool {
+	for _, obj := range physicalObjects {
+		if obj == self {
+			continue
+		}
+
+		if self.X < obj.X+obj.Width && self.X+self.Width > obj.X && self.Y+self.Height > obj.Y && self.Y < obj.Y+obj.Height {
+			return true
+		}
+	}
+	return false
+}
+
+func (self *PhysicalObj) CheckCollisionRight() bool {
+	for _, obj := range physicalObjects {
+		if obj == self {
+			continue
+		}
+
+		if self.X+self.Width > obj.X && self.X < obj.X+obj.Width && self.Y+self.Height > obj.Y && self.Y < obj.Y+obj.Height {
 			return true
 		}
 	}
--- src/player.go
+++ src/player.go
@@ -3,12 +3,17 @@ package WrdEngine
 import (
 	"fmt"
 	"github.com/veandco/go-sdl2/sdl"
-	"log"
 )
 
 type Player struct {
 	*PhysicalObj
-	velocityY int32
+	Jumping      bool
+	VelocityY    int32
+	Speed        float64
+	baseSpeed    float64
+	maxSpeed     float64
+	acceleration float64
+	deceleration float64
 }
 
 func NewPlayer(renderer *sdl.Renderer, imagePath string, x, y int32) (*Player, error) {
@@ -34,7 +39,11 @@ func NewPlayer(renderer *sdl.Renderer, imagePath string, x, y int32) (*Player, e
 				Renderer: renderer,
 			},
 		},
-		velocityY: 0,
+		baseSpeed:    10.0,
+		maxSpeed:     20.0,
+		acceleration: 0.5,
+		deceleration: 0.8,
+		VelocityY:    0,
 	}, nil
 }
 
@@ -45,29 +54,59 @@ func (self *Player) Tick(event sdl.Event) {
 	keyState[sdl.K_SPACE] = keys[sdl.SCANCODE_SPACE] != 0
 	keyState[sdl.K_a] = keys[sdl.SCANCODE_A] != 0
 	keyState[sdl.K_d] = keys[sdl.SCANCODE_D] != 0
-	self.PhysicTick()
-	const speed = 7
-	const jumpPower = -15
-	const gravity = 1
+	const speed = 10
+	const jumpPower = -70
 
-	log.Printf("Player: %v", *self)
-	log.Printf("OnGround? %v", self.OnGround())
 	if keyState[sdl.K_SPACE] && self.OnGround() && !self.Jumping {
-		log.Printf("Jumping, btw: %v", self)
 		self.Jumping = true
-		for i := 10; i > 0; i-- {
-			self.Y += jumpPower / 10
-		}
+		self.VelocityY = jumpPower
+	}
+
+	moving := false
+
+	if keyState[sdl.K_a] && self.X-int32(self.Speed) >= 0 && !self.CheckCollisionLeft() {
+		self.Speed = min(self.Speed+self.acceleration, self.maxSpeed)
+		self.ChangePos(self.X-int32(self.Speed), self.Y)
+		moving = true
+	}
+
+	if keyState[sdl.K_d] && self.X+int32(self.Speed) <= screen.Width-self.Width && !self.CheckCollisionRight() {
+		self.Speed = min(self.Speed+self.acceleration, self.maxSpeed)
+		self.ChangePos(self.X+int32(self.Speed), self.Y)
+		moving = true
+	}
+
+	if !moving {
+		self.Speed = max(self.Speed-self.deceleration, self.baseSpeed)
+	}
+}
+
+func min(a, b float64) float64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b float64) float64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// PhysicTick - Override method from PhysicalObject to add jumping and Velocity
+func (self *Player) PhysicTick() {
+	if !self.OnGround() {
+		self.VelocityY += int32(physic.GravtyPower)
+	} else if self.VelocityY > 0 {
+		self.VelocityY = 0
 		self.Jumping = false
 	}
 
-	self.velocityY += gravity
-	self.ChangePos(self.X, self.Y+self.velocityY)
+	self.Y += self.VelocityY
 
-	if keyState[sdl.K_a] && self.X-speed >= 0 {
-		self.ChangePos(self.X-speed, self.Y)
-	}
-	if keyState[sdl.K_d] && self.X+speed <= screen.Width-self.Width {
-		self.ChangePos(self.X+speed, self.Y)
+	if self.OnGround() {
+		self.Y = 600 - self.Height
 	}
 }
